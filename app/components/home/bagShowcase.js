e443dad3b2a8bcb96f12b1c949664790c095fd3f
rabby tn <rabby410@gmail.com>
2024-08-07 16:49:34 +0600

    bag showcase

--- app/components/home/bagShowcase.js
+++ app/components/home/bagShowcase.js
@@ -7,8 +7,11 @@ const BagShowcase = () => {
                 <div className="container mx-auto px-5">
                     <div className="text-center">
                         <p className='text-green-500 text-xl font-semibold'>We Love What We Do</p>
-                        <h2 className="text-3xl font-bold mt-3 text-blue-700">Our Handcrafted Some Products </h2>
-                        <h5 className="text-md mt-3">A client that&apos;s unhappy for a reason is a problem, a client that&apos;s unhappy though he or her can&apos;t quite a finger worse.</h5>
+                        <h2 className="text-3xl font-bold mt-3 text-blue-700">Our Handcrafted Bags </h2>
+                        <h5 className="text-md mt-3">
+                            A client that&apos;s unhappy for a reason is a problem,
+                            a client that&apos;s unhappy though he or her can&apos;t quite a finger worse.
+                        </h5>
                     </div>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 justify-items-center'>
                         <div className='flex flex-row md:flex-col gap-5 justify-self-end pr-5'>
--- app/components/home/banner.js
+++ app/components/home/banner.js
@@ -2,40 +2,40 @@ import Link from 'next/link'
 import React from 'react'
 
 const Banner = () => {
-  return (
-    <>
-    <div className="pb-5 bg-primary">
-        <div className="container mx-auto px-5">
-            <div className="grid grid-cols-1 lg:grid-cols-3">
-                <div className="flex flex-wrap pt-3 px-5 gap-5 md:border-r-2 border-secendary justify-center">
-                    <p className="text-3xl md:text-6xl font-semibold text-green-400">01.</p>
-                    <div className="w-[60%] group">
-                        <h2 className="text-md md:text-xl">CHOOSE PRODUCTS</h2>
-                        <p className="mt-2 text-sm md:text-lg">Browse for something you love and select the products you would like to have.</p>
-                        <div className="mt-5 group-hover:block text-sm md:text-lg"><Link href="" className="underline underline-offset-2 opacity-0 group-hover:opacity-100 transition-opacity">Read More</Link></div>
-                    </div>
-                </div>
-                <div className="flex flex-wrap pt-3 px-5 gap-5 md:border-r-2 border-secendary justify-center">
-                    <p className="text-3xl md:text-6xl font-semibold text-green-400">02.</p>
-                    <div className="w-[60%] group">
-                        <h2 className="text-md md:text-xl">PLACING ORDER</h2>
-                        <p className="mt-2 text-sm md:text-lg">Click on Buy Now Button | Provide us your detailed information.</p>
-                        <div className="mt-5 group-hover:block text-sm md:text-lg"><Link href="" className="underline underline-offset-2 opacity-0 group-hover:opacity-100 transition-opacity">Read More</Link></div>
-                    </div>
-                </div>
-                <div className="flex flex-wrap pt-3 px-5 gap-5 justify-center">
-                    <p className="text-3xl md:text-6xl font-semibold text-green-400">03.</p>
-                    <div className="w-[60%] group">
-                        <h2 className="text-md md:text-xl">HAPPY CUSTOMER</h2>
-                        <p className="mt-2 text-sm md:text-lg">Get the parcel in hand at your desired address | Make the payment.</p>
-                        <div className="mt-5 group-hover:block text-sm md:text-lg"><Link href="" className="underline underline-offset-2 opacity-0 group-hover:opacity-100 transition-opacity">Read More</Link></div>
+    return (
+        <>
+            <div className="pb-5 bg-primary">
+                <div className="container mx-auto px-5">
+                    <div className="grid grid-cols-1 lg:grid-cols-3">
+                        <div className="flex flex-wrap pt-3 px-5 gap-5 md:border-r border-opacity-30 border-secendary justify-center">
+                            <p className="text-3xl md:text-6xl font-semibold text-green-400">01.</p>
+                            <div className="w-[60%] group">
+                                <h2 className="text-md md:text-xl">CHOOSE PRODUCTS</h2>
+                                <p className="mt-2 text-sm md:text-lg">Browse for something you love and select the products you would like to have.</p>
+                                <div className="mt-5 group-hover:block text-sm md:text-lg"><Link href="" className="underline underline-offset-2 opacity-0 group-hover:opacity-100 transition-opacity">Read More</Link></div>
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap pt-3 px-5 gap-5 md:border-r border-opacity-30 border-secendary justify-center">
+                            <p className="text-3xl md:text-6xl font-semibold text-green-400">02.</p>
+                            <div className="w-[60%] group">
+                                <h2 className="text-md md:text-xl">PLACING ORDER</h2>
+                                <p className="mt-2 text-sm md:text-lg">Click on Buy Now Button | Provide us your detailed information.</p>
+                                <div className="mt-5 group-hover:block text-sm md:text-lg"><Link href="" className="underline underline-offset-2 opacity-0 group-hover:opacity-100 transition-opacity">Read More</Link></div>
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap pt-3 px-5 gap-5 justify-center">
+                            <p className="text-3xl md:text-6xl font-semibold text-green-400">03.</p>
+                            <div className="w-[60%] group">
+                                <h2 className="text-md md:text-xl">HAPPY CUSTOMER</h2>
+                                <p className="mt-2 text-sm md:text-lg">Get the parcel in hand at your desired address | Make the payment.</p>
+                                <div className="mt-5 group-hover:block text-sm md:text-lg"><Link href="" className="underline underline-offset-2 opacity-0 group-hover:opacity-100 transition-opacity">Read More</Link></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Banner
